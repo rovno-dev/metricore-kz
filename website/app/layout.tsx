295d59d9b1e8de2ee1374a0917631c271a21f17f
niyazgim <niiaz280228@gmail.com>
2026-08-06 16:11:40 +0300

    fix errors

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -4,8 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip"
 import { cn } from "@/lib/utils";
 import localFont from 'next/font/local'
 import { ThemeProvider } from "@/providers/theme-provider";
-import { WishlistProvider } from "@/providers/wishlist-provider";
-import { AuthProvider } from "@/providers/auth-provider";
 import BottomAppBar from "@/components/layout/bottom-app-bar";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
@@ -85,20 +83,16 @@ export default function RootLayout({
         />
       </head>
       <body className="flex flex-col min-h-[100dvh]">
-        <AuthProvider>
-          <ThemeProvider>
-            <TooltipProvider>
-              <WishlistProvider>
-                <Header />
-                <main className="flex-1 pt-[56px]">
-                  {children}
-                </main>
-                <Footer />
-                {/* <BottomAppBar /> */}
-              </WishlistProvider>
-            </TooltipProvider>
-          </ThemeProvider>
-        </AuthProvider>
+        <ThemeProvider>
+          <TooltipProvider>
+            <Header />
+            <main className="flex-1 pt-[56px]">
+              {children}
+            </main>
+            <Footer />
+            {/* <BottomAppBar /> */}
+          </TooltipProvider>
+        </ThemeProvider>
       </body>
     </html >
   );
